09af2958e753d9d11abef9d65bd63b13fd0749d8
mingu62-lee <135577364+mingu62-lee@users.noreply.github.com>
2024-04-05 16:34:54 +0900

    Update solution.py day2

--- 2023/day2/solution.py
+++ 2023/day2/solution.py
@@ -1,19 +1,39 @@
 import os
+import re
+
 def openfile():
     file_list = []
-    with open('./2023/day1/input.txt') as file_data:
+    with open('./input.txt') as file_data:
         for line in file_data.readlines():
             file_list.append(line.split("\n")[0])
         return file_list
-    
+
+
 def part1_solution():
-    pass
+    game = openfile()
+    answer = 0
+    for inp in game:
+        ID = inp.split(":")[0].split(" ")[1]
+        cube_dict = {"red":(list(map(int,(re.findall(r'(\d+) red\b', inp))))), "green":(list(map(int,(re.findall(r'(\d+) green\b', inp))))), "blue": (list(map(int,(re.findall(r'(\d+) blue\b', inp)))))}
+        if max(cube_dict["red"]) > 12 or max(cube_dict["green"]) > 13 or max(cube_dict["blue"]) > 14:
+            pass
+        else:
+            answer += int(ID)
+            
+    return answer
+        
 
 def part2_solution():
-    pass
+    game = openfile()
+    answer = 0
+    for inp in game:
+        ID = inp.split(":")[0].split(" ")[1]
+        cube_dict = {"red":(list(map(int,(re.findall(r'(\d+) red\b', inp))))), "green":(list(map(int,(re.findall(r'(\d+) green\b', inp))))), "blue": (list(map(int,(re.findall(r'(\d+) blue\b', inp)))))}
+        x = max(cube_dict["red"]) * max(cube_dict["green"]) * max(cube_dict["blue"])
+        answer += x
+    return answer
 
 if __name__ == "__main__":
     ret1 = part1_solution()
-    #ret2 = part2_solution()
-    print(ret1)
-    #print(ret2)
+    ret2 = part2_solution()
+    print(ret2)
